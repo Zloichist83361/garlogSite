7c147c1c1590d0b4d60899c553cec0f1f9554c48
Zloichist83361 <n.chistyak0v322@gmail.com>
2021-07-23 16:39:16 +0300

    order 4

--- order/views.py
+++ order/views.py
@@ -1,6 +1,10 @@
+import json
+
 from django.shortcuts import redirect, render
 
 from order.forms import OrderForm
+
+from calculator.models import Calculate
 
 # Create your views here.
 
@@ -20,3 +24,13 @@ def thanks(request):
          'is_worker': request.user.groups.filter(name='worker').exists(),
     }
     return render(request, 'thanks.html', context)
+
+
+def my_data(request):
+    data_form = request.POST.get('data_form')
+    max_weight = max(data_form[2], data_form[3]*240)
+    response = Calculate.objects.filter(cityto__exact=data_form[0]).filter(cityfrom__exact=data_form[1]).filter(weightto__gte=max_weight).filter(weightfrom__lte=max_weight)
+    res_order = {
+        'my_data': response
+    }
+    return json.dumps(res_order)
